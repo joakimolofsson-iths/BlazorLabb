912673af1f2da54cf29427696fbf6824baf222a0
joakimolofsson-iths <joakim.olofsson@iths.se>
2024-10-24 23:23:10 +0200

    Created Users.razor. Moved the classes in NewUser.razor to the Models folder.

---
+++ BlazorLabb/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorLabb.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Address UserAddress { get; set; } = new Address();
+        public Company UserCompany { get; set; } = new Company();
+    }
+}
